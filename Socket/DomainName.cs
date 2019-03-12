4772284f67f393f1eee5ccf9ddd910cb7420a8ce
SzilvasiPeter <peti.szilvasi95@gmail.com>
2019-03-12 18:35:22 +0100

    Get ip adress of a domain name Get hostname from ip adress

---
+++ Socket/DomainName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+
+namespace Socket
+{
+    class DomainName
+    {
+        static void Main(string[] args)
+        {
+            IPHostEntry host1 = Dns.GetHostEntry("www.microsoft.com");
+            foreach (IPAddress ip in host1.AddressList)
+            {
+                Console.WriteLine(ip.ToString());
+            }
+            IPHostEntry host2 = Dns.GetHostEntry("91.120.22.150");
+            Console.WriteLine(host2.HostName);
+        }
+    }
+}
