014bdef025c1bdfd00babec890eb28d24f4528a7
SzilvasiPeter <peti.szilvasi95@gmail.com>
2019-03-14 16:27:45 +0100

    LINQ to obeject outer join using DefaultIfEmpty method

--- LINQ2/ListJoin.cs
+++ LINQ2/ListJoin.cs
@@ -16,15 +16,28 @@ namespace CSharp.LINQ2
                     Name = customer.FirstName + " " + customer.LastName,
                     Products = DataClass.GetProductList().Where(product => (order.ProductID == product.ID))
                 });
-            //System.Console.WriteLine("Count: " + result.Count() + "Length: " + result.ElementAt(0) + result.ElementAt(1) + result.ElementAt(2));
-            foreach (var orders in result)
-            {
-                Console.WriteLine(orders.Name);
-                foreach (var product in orders.Products)
+
+            var result1 = from customer in DataClass.GetCustomerList()
+                join order in DataClass.GetOrderList()
+                on customer.ID equals order.CustomerID into tmpresult
+                from o in tmpresult.DefaultIfEmpty()
+                select new
                 {
-                    Console.WriteLine("-- {0}", product.ProductName);
-                }
+                    Name = customer.FirstName + " " + customer.LastName,
+                    Product = o == null ? "no order" : o.ProductID.ToString()
+                };
+            //System.Console.WriteLine("Count: " + result.Count() + "Length: " + result.ElementAt(0) + result.ElementAt(1) + result.ElementAt(2));
+            foreach(var order in result1){
+                System.Console.WriteLine("{0}: {1}", order.Name, order.Product);
             }
+            // foreach (var orders in result)
+            // {
+            //     Console.WriteLine(orders.Name);
+            //     foreach (var product in orders.Products)
+            //     {
+            //         Console.WriteLine("-- {0}", product.ProductName);
+            //     }
+            // }
         }
     }
 }
